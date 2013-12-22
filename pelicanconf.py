7f665c9813af7565847d90c214b921239b74354b
Silviu Tantos <me@razius.com>
2013-12-22 11:19:48 +0100

    Changed url paths.

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,25 +49,28 @@ EXTRA_PATH_METADATA = {
 }
 
 # URL settings
-# PAGINATION_PATTERNS = (
-#     (1, '{base_name}/', '{base_name}/index.html'),
-#     (2, '{base_name}/page/{number}/', '{base_name}/articles/page/{number}/index.html'),
-# )
-ARTICLE_URL = ('articles/{slug}.html')
-ARTICLE_SAVE_AS = ('articles/{slug}.html')
-PAGE_URL = ('pages/{slug}.html')
-PAGE_SAVE_AS = ('pages/{slug}.html')
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/articles/page/{number}/index.html'),
+)
+ARTICLE_URL = ('articles/{slug}/')
+ARTICLE_SAVE_AS = ('articles/{slug}/index.html')
+PAGE_URL = ('pages/{slug}/')
+PAGE_SAVE_AS = ('pages/{slug}/index.html')
 PAGE_LANG_SAVE_AS = False
-TAG_URL = ('tag/{slug}.html')
-TAG_SAVE_AS = ('tag/{slug}.html')
-TAGS_URL = ('tag/index.html')
-TAGS_SAVE_AS = ('tag/index.html')
+TAG_URL = ('tag/{slug}/')
+TAG_SAVE_AS = ('tag/{slug}/index.html')
+TAGS_URL = ('tags/')
+TAGS_SAVE_AS = None
+CATEGORY_URL = ('category/{slug}/')
+CATEGORY_SAVE_AS = ('category/{slug}/index.html')
 AUTHOR_SAVE_AS = False
 
 # Feed.
 FEED_DOMAIN = SITEURL
-FEED_ALL_RSS = 'feeds/all.rss.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
 
 # Theme.
 THEME = 'theme'
